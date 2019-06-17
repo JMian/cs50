1ec79ab1a05488ab11274278005036dbc4d53ff0
JMian <JMian@users.noreply.github.com>
2019-06-17 10:04:04 +0000

    20190617T100313Z

--- cash.c
+++ cash.c
@@ -2,37 +2,34 @@
 #include <cs50.h>
 #include <math.h>
 
-float get_positive_float(string prompt);
 int quarters, dimes, nickels, pennies;
 
 int main(void)
 {
-    float dollars = get_positive_float("Change owed: ");
-    int cents = round(dollars * 100);
-    for (quarters = 0; cents >= 25; quarters++)
+    float dollars;
+    do
     {
-        cents -= 25; 
+        dollars = get_float("Change owed: ");
     }
-    for (dimes = 0; cents >= 10; dimes++)
+    while (dollars < 0);
+   
+    int cents = round(dollars * 100);
+    
+    while (cents >= 25)
     {
+        quarters++;
+        cents -= 25;
+    }
+    while (cents >= 10)
+    {
+        dimes++;
         cents -= 10;
     }
-    for (nickels = 0; cents >= 5; nickels++)
+    while (cents >=5)
     {
+        nickels++;
         cents -= 5;
     }
     pennies = cents;
     printf("%i\n", quarters + dimes + nickels + cents);  
-}
-
-
-float get_positive_float(string prompt)
-{
-    float n;
-    do
-    {
-      n = get_float("%s", prompt); 
-    }
-    while (n < 0);
-    return n;
 }
